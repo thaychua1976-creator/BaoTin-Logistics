c49c067adf99cf4d59135a214484c4b1c4cf6f69
ThayChua <thaychua1976@gmail.com>
2026-07-24 15:25:26 +0700

    Sua phan export chuyen di

--- views/chuyen_di.py
+++ views/chuyen_di.py
@@ -136,12 +136,12 @@ with tab1:
                 # Định dạng nội dung tin nhắn Zalo chuẩn
                 noi_dung_chat = (
                     f"LỆNH ĐIỀU XE BẢO TÍN \n"
-                    f"Mã chuyến: {row['Mã']}\n"
-                    f"Ngày chạy: {row['Ngày']}\n"
-                    f"Khách hàng: {row['Khách hàng']}\n"
-                    f"Lộ trình: {row['Lộ trình']}\n"
-                    f"Ghi chú: {ghi_chu}\n"
-                    f"Vui lòng chuẩn bị hàng hóa!"
+                    f" Mã chuyến: {row['Mã']}\n"
+                    f" Ngày chạy: {row['Ngày']}\n"
+                    f" Khách hàng: {row['Khách hàng']}\n"
+                    f" Lộ trình: {row['Lộ trình']}\n"
+                    f" Ghi chú: {ghi_chu}\n"
+                    f" Vui lòng chuẩn bị hàng hóa!"
                 )
                 
                 danh_sach_zalo.append({
